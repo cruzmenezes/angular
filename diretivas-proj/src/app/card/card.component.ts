dc75647c786f6b1b5eb8bcc754a59397eab25283
Edson_Victor <cruz.menezes@outlook.com>
2023-05-18 23:40:01 -0300

    Single Page Application com Angular

--- diretivas-proj/src/app/card/card.component.ts
+++ diretivas-proj/src/app/card/card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-card',
@@ -6,5 +6,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./card.component.css']
 })
 export class CardComponent {
+  produtos : string[] = []
+  menuType:string = ""
 
-}
+
+    constructor(){
+      this.produtos = [
+        "Produto 1",
+        "Produto 2",
+        "Produto 3",
+
+      ]
+    }
+
+    adicionar(){
+      this.produtos.push("Victor")
+
+    }
+
+    remover(){
+      this.produtos.pop()
+    }
+
+    remover2(index: number){
+      this.produtos.splice(index, 1)
+    }
+    }
+
+
+
