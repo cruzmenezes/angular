7e8597803f319f6d9cbbcec7c1139c68fdb284a0
Edson_Victor <cruz.menezes@outlook.com>
2023-05-20 21:16:27 -0300

    declaração modulos

--- diretivas-proj/src/app/app.module.ts
+++ diretivas-proj/src/app/app.module.ts
@@ -1,19 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import {ListaModule} from './lista/lista.module'
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CardComponent } from './card/card.component';
+import {ListaModule } from './lista/lista.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CardComponent
+    CardComponent,
+    ListagemComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ListaModule
   ],
+  exports:[],
   providers: [],
   bootstrap: [AppComponent]
 })
